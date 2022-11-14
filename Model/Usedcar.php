d09bd3c976d306d98a3cdfc16e732d7dea057775
mohamed maher <mohamedmaher.grati@esprit.tn>
2022-11-14 13:46:39 +0100

    Model

--- Model/Usedcar.php
+++ Model/Usedcar.php
@@ -4,8 +4,17 @@ namespace Model;
 
 class Usedcars
 {
-    
-    public int $regNum;
+    public int $id;
+    public string $category;
+    public string $brand;
+    public string $model;
+    public int $cylinder;
+    public string $energy;
+    public int $fiscalPower;
+    public string $gearbox;
+    public string $registerNumber;
     public int $year;
     public int $kilometers;
+    public int $rate;
+
 }
